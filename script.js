a0466faf52bb2b7fa490b853dfaec1921aff077c
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2022-05-22 17:00:12 -0400

    almost complete, bug with scissors

---
+++ script.js
@@ -0,0 +1,40 @@
+let playerSelection = prompt("Please enter rock, paper, or scissors: ");
+//console.log(playerSelection)
+function computerPlay() {
+    let random = Math.floor(Math.random()* 3) + 1;
+    //console.log(com)
+    if (random === 1) {
+        computerSelection = "Rock";
+    } else if (random === 2) {
+        computerSelection = "Paper";
+    } else if (random === 3){
+        computerSelection =  "Scissors";
+    }
+    return computerSelection;
+}
+//console.log(computerPlay())
+function play(computerSelection, playerSelection) {
+    computerSelection = computerSelection.toUpperCase();
+    //console.log(computerSelection)
+    playerSelection = playerSelection.toUpperCase();
+    //console.log(playerSelection)
+    let areEqual = computerSelection === playerSelection;
+    //console.log(areEqual)
+    if (areEqual === 1) {
+        return "Tie! Play again.";
+    } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+        return "Rock beats scissors! The computer wins. Play again.";
+    } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+        return "Paper beats rock! The computer wins. Play again.";
+    } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+        return "Scissors beat paper! The computer wins. Play again.";
+    } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+        return "Paper beats rock! You win. Play again.";
+    } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+        return "Scissors beat paper! You win. Play again.";
+    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+        return "Rock beats scissors! You win. Play again.";
+    } 
+}
+console.log(computerPlay())
+console.log(play(computerSelection,playerSelection))
