25c20bb888fe3993087417baa31ec78af0c4ebca
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2022-05-22 22:25:01 -0400

    added score tally

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+ //keeping score
+ let wins = 0;
+ let losses = 0;
+ let ties = 0;
+ 
 for(let i = 0; i < 5; i++) {
     //getting user input
     let playerSelection = prompt("Please enter rock, paper, or scissors: ");
@@ -17,29 +22,41 @@ for(let i = 0; i < 5; i++) {
     console.log(playerSelection)
     console.log(computerSelection)
 
+   
+
     function play(computerSelection, playerSelection) {
         // checking for tie
         computerSelection = computerSelection.toUpperCase();
         playerSelection = playerSelection.toUpperCase();
         let areEqual = computerSelection === playerSelection;
         
+        
+
         //evaluating answers to find result of game
         if (areEqual === true) {
+            ties++;
             return "Tie! Play again.";
         } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+            losses++;
             return "Rock beats scissors! The computer wins. Play again.";
         } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+            losses++;
             return "Paper beats rock! The computer wins. Play again.";
         } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+            losses++;
             return "Scissors beat paper! The computer wins. Play again.";
         } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+            wins++;
             return "Paper beats rock! You win. Play again.";
         } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+            wins++;
             return "Scissors beat paper! You win. Play again.";
         } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+            wins++;
             return "Rock beats scissors! You win. Play again.";
         } 
     }
     console.log(play(computerSelection,playerSelection))
 }
+console.log("Wins: " + wins + ", Losses: " + losses + ", and Ties: " + ties) 
         
